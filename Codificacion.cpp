6a5703e0e4104fae0f908d8d079ace48c92a0858
Ingrid <131918653+ingridddts@users.noreply.github.com>
2024-06-16 20:07:26 -0400

    Update Codificacion.cpp
    estructura para el árbol

--- Codificacion.cpp
+++ Codificacion.cpp
@@ -1,5 +1,20 @@
-int main() {
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <string>
 
+using namespace std;
+
+// Estructura para representar un nodo en el árbol Huffman
+struct NodoHuffman {
+    char caracter;
+    int frecuencia;
+    NodoHuffman *izquierda, *derecha;
+
+    NodoHuffman(char c, int freq) : caracter(c), frecuencia(freq), izquierda(nullptr), derecha(nullptr) {}
+};
+
+int main() {
 
     return 0;
 }
